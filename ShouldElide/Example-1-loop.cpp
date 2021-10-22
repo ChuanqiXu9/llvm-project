698e7c5bb5b40fda2c8a1a9731529d32d46f9255
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2021-10-22 09:47:13 +0800

    Remove commit marker

--- ShouldElide/Example-1-loop.cpp
+++ ShouldElide/Example-1-loop.cpp
@@ -8,16 +8,11 @@ AlwaysElideTask an_elided_coroutine() {
 
 NormalTask example1(int count) {
   int sum = 0;
-<<<<<<< HEAD
-  for (int i = 0; i < count; ++i)
-    sum += co_await an_elided_coroutine();
-=======
   for (int i = 0; i < count; ++i) {
     auto t = an_elided_coroutine();
     assert (t.Elided());
     sum += co_await std::move(t);
   }
->>>>>>> Updating tests and fix bugs in impl
 
   co_return sum;
 }
